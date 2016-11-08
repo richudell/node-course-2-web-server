61f6b30af53abc7f18ab6af60579345e28f7a1b3
Rich Udell <rudell@proshipinc.com>
2016-11-08 08:18:18 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}:L ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log.')
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+});
+
+app.get('/', (req, res) => {
+//  res.send('<H1>Hello Express!</H1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to the future'
+  });
+});
+
+app.get('/about', (req, res) =>{
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) =>{
+  res.send({
+    errCode: 4503,
+    errMessage: 'Now you did it'
+  })
+});
+
+app.listen(3333, () => {
+  console.log('Server is up on port 3333');
+});
